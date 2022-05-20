2a8bf4159cdd1decb9de43e982192b989a49ac43
August Høyen Moum <97290389+aumoum@users.noreply.github.com>
2022-05-20 14:11:51 +0200

    Add code for databricks jobs deployment (#10)
    * added print on job creation
    
    * add print errorcode on error
    
    * updated order to avoid bugs
    
    * fix
    
    * minor change
    
    * update schedule function
    
    * updated add_clusterid func
    
    * minor change
    
    * print api status code
    
    * remove sleep
    
    * fix comments
    
    * minor change
    
    * added gitignore
    
    * minor changes
    
    * debug: print jobs
    
    * debugging
    
    * minor change
    
    * debugging prints
    
    * debug
    
    * debugging2
    
    * add condition when changing schedule
    
    * Jobs: Added environment as input
    
    * added environment for deciding schedule
    
    * fix
    
    * fix
    
    * fix comments

--- Source/Deploy/deploy-files/databrick_jobs.py
+++ Source/Deploy/deploy-files/databrick_jobs.py
@@ -1,74 +1,35 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
-from pkgutil import get_data
 import sys
 import json
 import os
 from azure.identity import AzureCliCredential
 from azure.keyvault.secrets import SecretClient
-from click import echo
-from requests import get
 
 from DatabricksApi.Jobs import DatabricksJobsAPI
-import time
 
 
 def get_databricks_secrets_keyvault(keyvault_url, secretName):
     '''
-    Get secrets from keyVault 
+    Get secrets from keyVault
     :param
         keyvault_url(str): The keyvault to use
-
-    :param
-        secretName(str): Secretname you wanna return 
-
+        secretName(str): Name of secret to return
     '''
     try:
         credential = AzureCliCredential()
         client = SecretClient(vault_url=keyvault_url, credential=credential)
-        secret =client.get_secret(secretName)
+        secret = client.get_secret(secretName)
         return secret.value
     except Exception as e:
         print(e)
-       
+
 
 def add_clusterId(dir, cluster_id):
-        '''
-     Updates the Json file with correct Cluster Id.
-
-    :param dir: 
-        
-    :param cluster_id:
-        Cluster_Id(str): Returns the clusterid to use
-    ''' 
-        try:  
-            for filename in os.listdir(dir):
-                if filename.endswith(".json"):
-                    file = (os.path.join(dir, filename))
-                    with open(file, 'r+') as f:
-                        json_object = json.load(f)
-                    
-                    f.close()
-                    if 'tasks' in json_object:
-                        for i in json_object['tasks']:
-                            for key in i:
-                                if key == 'existing_cluster_id':
-                                    i[key] = cluster_id
-                    elif 'existing_cluster_id' in json_object:
-                        json_object['existing_cluster_id'] = cluster_id
-                with open(file, 'w') as f:
-                    json.dump(json_object, f,indent=4)
-        except Exception as e:
-            print(e)
-
-def update_schedule(dir, keyurl):
     '''
-    Updates the json file with PAUSED or UNPASUED depends on envirnonmet
-
-    :param dir:
-        path to files
-    :param keyurl:
-        Keyvault to use
+    :param
+        dir: Updates the Json file with correct cluster Id
+        cluster_id(str): Returns the cluster id to use
     '''
     try:
         for filename in os.listdir(dir):
@@ -78,76 +39,128 @@ def update_schedule(dir, keyurl):
                     json_object = json.load(f)
                 f.close()
 
-                url = keyurl.split("-")
-                if url[2] in ["prod","qa" ]:
-                    status = 'UNPAUSED' 
-                    json_object['schedule']['pause_status'] = status
-                else:
-                    status = "PAUSED"
-                    json_object['schedule']['pause_status'] = status
+                if 'tasks' in json_object:
+                    for i in json_object['tasks']:
+                        for key in i:
+                            if key == 'existing_cluster_id':
+                                i[key] = cluster_id
+                elif 'existing_cluster_id' in json_object:
+                    json_object['existing_cluster_id'] = cluster_id
+            # Save our changes to JSON file
             with open(file, 'w') as f:
-                    json.dump(json_object, f, indent=4)
+                json.dump(json_object, f, indent=4)
+            f.close()
+
     except Exception as e:
-        print(e)    
+        print(e)
+
+
+def update_schedule(dir, env):
+    '''
+    Updates the json file with PAUSED or UNPASUED depends on environment
+    :param 
+        dir: path to files
+        env: environment to deploy to
+    '''
+    try:
+        for filename in os.listdir(dir):
+            if filename.endswith(".json"):
+                file = (os.path.join(dir, filename))
+                with open(file, 'r+') as f:
+                    json_object = json.load(f)
+                f.close()
+
+                if('schedule' in json_object):
+                    if env in ["sbox", "dev", "test"]:
+                        status = 'PAUSED'
+                        json_object['schedule']['pause_status'] = status
+                    else:
+                        status = "UNPAUSED"
+                        json_object['schedule']['pause_status'] = status
+
+            # Save our changes to JSON file
+            with open(file, 'w') as f:
+                json.dump(json_object, f, indent=4)
+            f.close()
+
+    except Exception as e:
+        print(e)
+
 
 def get_local_jobs(folder):
     '''
     Return files from input folder
-
-    :param folder:
-       string    
+    :param 
+        folder: string    
     '''
     try:
         local_jobs = []
         for file in [os.path.join(folder, filename) for filename in os.listdir(folder)]:
             with open(file, 'r') as f:
                 local_jobs.append(json.loads(f.read()))
+            f.close()
         return local_jobs
+
     except Exception as e:
         print(e)
+
+
+# _TODO: Only update if theres any change
+
 
 def upsert_jobs(databricks_jobs, local_jobs, cluster_jobs):
     cluster_job_names = [job['settings']['name'] for job in cluster_jobs]
     cluster_jobs_dict = databricks_jobs.get_jobs_dict()
 
     if local_jobs is None:
-        return  
+        print('No local jobs found')
+        return
     for local_job in local_jobs:
         try:
             if local_job['name'] not in cluster_job_names:
-                databricks_jobs.create_job(local_job)
                 print(f"Creating job '{local_job['name']}'")
-            else:  ##TODO: Only update if theres any change
+                databricks_jobs.create_job(local_job)
+            else:
                 cluster_job_id = cluster_jobs_dict[local_job['name']]
-                databricks_jobs.update_job(local_job, cluster_job_id)
                 print(f"Updating job '{local_job['name']}'")
+                databricks_jobs.update_job(local_job, cluster_job_id)
+
         except Exception as e:
             continue
 
+
 def main():
+
     cluster_id_name = 'atlas-databricks-maiacmn-%s-id' % sys.argv[1].lower()
     keyvault_url = 'https://%s.vault.azure.net/' % sys.argv[2]
+    environment = sys.argv[3]
+
+    local_job_folder = './artifact/Databricks-jobs/'
 
     # Get keys from vault
-    dbr_token = get_databricks_secrets_keyvault(keyvault_url,'atlas-maiacmn-databricks-pat')
-    dbr_url = get_databricks_secrets_keyvault(keyvault_url, 'atlas-maiacmn-databricks-url')
+    dbr_token = get_databricks_secrets_keyvault(
+        keyvault_url, 'atlas-maiacmn-databricks-pat')
+    dbr_url = get_databricks_secrets_keyvault(
+        keyvault_url, 'atlas-maiacmn-databricks-url')
     cluster_id = get_databricks_secrets_keyvault(keyvault_url, cluster_id_name)
 
     databricks_jobs = DatabricksJobsAPI(dbr_url, dbr_token)
 
-    # Fetch jobs
-    folder = './artifact/Databricks-jobs/'
-    
-    # Get jobs from folder in repository 
-    local_jobs = get_local_jobs(folder)
+    # Update schedule and pause schedules if environment is dev or test.
+    update_schedule(local_job_folder, environment)
+    # Update clusterId based on cluster_id retrieved from key vault.
+    add_clusterId(local_job_folder, cluster_id)
+
+    # Fetch jobs from repository
+    local_jobs = get_local_jobs(local_job_folder)
+
     # Get jobs from databricks workspace
     cluster_jobs = databricks_jobs.get_jobs()
 
-    # Create and Update jobs
-    add_clusterId('./artifact/Databricks-jobs/', cluster_id)
-    update_schedule('./artifact/Databricks-jobs/', keyvault_url)
-    time.sleep(5.5)
+    # Create and update jobs
+    print('Local jobs used for updating:' + str(local_jobs))
     upsert_jobs(databricks_jobs, local_jobs, cluster_jobs)
+
 
 if __name__ == "__main__":
     main()
